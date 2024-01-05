71337c1c2a386f1c7d1882dc6739b2894a1f7fcb
wileland <wileland7@gmail.com>
2024-01-04 23:22:51 -0600

    Update MongoDB URI and add devDependencies

--- server.js
+++ server.js
@@ -9,11 +9,14 @@ const PORT = process.env.PORT || 3000;
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(routes);
 
-// Connect to MongoDB and start the server
+// Set up a prefix for all routes defined in the routes directory
+app.use("/api", routes);
+
+// Connect to MongoDB
 connectDB()
   .then(() => {
+    // Only start the server if the database connection is successful
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
